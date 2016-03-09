eccf9234099f9543aae4e060953dc13c5fae9b43
Hank Yates <hankyates@gmail.com>
2016-03-08 20:00:18 -0800

    retab and and rename rotues -> routes

--- server/publications.js
+++ server/publications.js
@@ -3,15 +3,15 @@
 //****************
 
 Meteor.publish('polls', function () {
-	check(arguments, [Match.Any]);
-	return Polls.find({});
+  check(arguments, [Match.Any]);
+  return Polls.find({});
 });
 
 
 Meteor.publish('pollDetails', function (pollId) {
-	check(pollId, String);
-	return [
-		Polls.find({_id: pollId}),
-		Votes.find({pollId: pollId})
-	];
+  check(pollId, String);
+  return [
+    Polls.find({_id: pollId}),
+    Votes.find({pollId: pollId})
+  ];
 });
